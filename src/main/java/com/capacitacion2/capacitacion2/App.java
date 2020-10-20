1c7681d1e1a8d73b76c75d3de5500e02d93787e6
Eder Leandro Carbonero <eventsequor@gmail.com>
2020-10-20 07:51:40 -0500

    primer commit xx2

---
+++ src/main/java/com/capacitacion2/capacitacion2/App.java
@@ -0,0 +1,21 @@
+package com.capacitacion2.capacitacion2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String opcion = "opcion2";
+        switch (opcion) {
+		case "opcion2":
+			
+			break;
+
+		default:
+			break;
+		}
+    }
+}
