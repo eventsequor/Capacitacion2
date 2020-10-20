8eebff5a6c6c0b44d6ea2c6156a36275c1b8bf64
Eder Leandro Carbonero <eventsequor@gmail.com>
2020-10-20 07:59:29 -0500

    Cambio en el main

--- src/main/java/com/capacitacion2/capacitacion2/App.java
+++ src/main/java/com/capacitacion2/capacitacion2/App.java
@@ -8,14 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String opcion = "opcion2";
-        switch (opcion) {
-		case "opcion2":
-			
-			break;
-
-		default:
-			break;
-		}
+    	System.out.println("");
     }
 }
