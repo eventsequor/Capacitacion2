975a188cb232061c619dc4a36459df035cc38728
Eder Leandro Carbonero <eventsequor@gmail.com>
2020-10-27 08:02:32 -0500

    Clase 4

--- src/main/java/com/capacitacion2/capacitacion2/App.java
+++ src/main/java/com/capacitacion2/capacitacion2/App.java
@@ -1,6 +1,8 @@
 package com.capacitacion2.capacitacion2;
 
 import com.capacitacion2.capacitacion2.clase3.AutomatizacionPaginaWeb;
+import com.capacitacion2.capacitacion2.clase4.ManagerDriver;
+import com.capacitacion2.capacitacion2.clase4.PageObjectoTourFrancia;
 
 /**
  * Hello world!
@@ -10,7 +12,22 @@ public class App
 {
     public static void main( String[] args )
     {
-    	AutomatizacionPaginaWeb objLanzaPagina = new AutomatizacionPaginaWeb();
-    	objLanzaPagina.interacturasConGmail();
+    	String urlPagina = "https://www.marca.com/ciclismo/tour-francia/clasificacion.html";
+    	ManagerDriver objManager = new ManagerDriver("google", urlPagina);
+    	objManager.navegarAPagina();
+    	PageObjectoTourFrancia objTour = new PageObjectoTourFrancia(objManager.getWebDriver());
+    	objTour.seleccionListaDesplegable("Etapa 17");
+    	espera(3);
+    	objTour.imprimirListaCiclistas();
+    	espera(5);
+    	objManager.cerrarNavegador();
+    }
+    
+    public static void espera(int tiempo) {
+    	try {
+			Thread.sleep(tiempo*1000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
     }
 }
